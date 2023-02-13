af8ed3deb4ca9e8fecd33e3c5a1652ab226abfb9
Jwander <jasper870820@gmail.com>
2023-02-14 00:06:29 +0800

    feat: 新增添加文字處理、新增指定顏色輸出圖片
    1. 新增添加文字處理，底部塗以白底黑字呈現文字內容，文字內容放置RGB、CMYK、HSV、HSL、HEX等資料
    2. generate_img新增生成指定顏色功能，並添加設定設定是否顯示文字內容以及是否儲存圖片

--- core/generate_color_img.py
+++ core/generate_color_img.py
@@ -1,6 +1,6 @@
 import random
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 from utils.color_converter import ColorConverter
 
@@ -10,6 +10,31 @@ class GenerateColorImg:
         self.color_img = None
         self.width = 1200
         self.height = 1200
+
+    def generate_img(self, color=None, add_text=True, save_img=False):
+        """
+        生成純色圖片
+        :param color: 指定顏色，需為元組形式，預設為None，生成隨機顏色
+        :param add_text: 是否在底部添加說明文字，預設為True
+        :param save_img: 是否儲存圖片，預設為False
+        :return:
+        """
+        if not color:
+            self.rgb_color = self.get_random_color()  # (r, g, b) 隨機顏色
+        else:
+            self.rgb_color = color  # 指定顏色
+        self.color_img = Image.new("RGB", (self.width, self.height), self.rgb_color)  # 建立純色圖片
+        self.cmyk_color, self.hsv_color, self.hsl_color, self.hex_color = self.get_color_convert_result(self.rgb_color)
+        print(f"RGB: {self.rgb_color}")
+        print(f"CMYK: {self.cmyk_color}")
+        print(f"HSV: {self.hsv_color}")
+        print(f"HSL: {self.hsl_color}")
+        print(f"HEX: {self.hex_color}")
+        if add_text:
+            self.add_text()
+        if save_img:
+            self.save_color_img()
+        return self.color_img
 
     def get_random_color(self):
         r = random.randint(0, 255)
@@ -24,21 +49,45 @@ class GenerateColorImg:
         hex_color = ColorConverter.rgb_to_hex(rgb_color)
         return cmyk_color, hsv_color, hsl_color, hex_color
 
-    def generate_img(self):
-        rgb_color = self.get_random_color()  # r, g, b
-        self.color_img = Image.new("RGB", (self.width, self.height), rgb_color)
-        cmyk_color, hsv_color, hsl_color, hex_color = self.get_color_convert_result(rgb_color)
+    def add_text(self):
+        # 基本設定
+        draw = ImageDraw.Draw(self.color_img)
+        font = ImageFont.truetype("arialbd.ttf", 40, encoding='utf-8')  # 設定字型
+        font_color = (0, 0, 0)  # 黑色
+        # 添加底部空白說明欄
+        left_top = (0, 1000)
+        right_bottom = (1200, 1200)
+        fill_color = (255, 255, 255)  # 白色
+        draw.rectangle((left_top, right_bottom), fill=fill_color)
+        # 添加文字
+        r, g, b = self.rgb_color
+        text = f"RGB: ({r}, {g}, {b})"
+        position = (20, 1020)
+        draw.text(position, text, fill=font_color, font=font)
 
-        print(f"RGB: {rgb_color}")
-        print(f"CMYK: {cmyk_color}")
-        print(f"HSV: {hsv_color}")
-        print(f"HSL: {hsl_color}")
-        print(f"HEX: {hex_color}")
-        return self.color_img
+        c, m, y, k = self.cmyk_color
+        text = f"CMYK: ({c}%, {m}%, {y}%, {k}%)"
+        position = (600, 1020)
+        draw.text(position, text, fill=font_color, font=font)
+
+        h, s, v = self.hsv_color
+        text = f"HSV: ({h}°, {s}%, {v}%)"
+        position = (20, 1080)
+        draw.text(position, text, fill=font_color, font=font)
+
+        h, s, l = self.hsl_color
+        text = f"HSL: ({h}°, {s}%, {l}%)"
+        position = (600, 1080)
+        draw.text(position, text, fill=font_color, font=font)
+
+        text = f"HEX: {self.hex_color}"
+        position = (20, 1140)
+        draw.text(position, text, fill=font_color, font=font)
 
     def save_color_img(self):
         self.color_img.save("random_image.png")
 
 
 if __name__ == "__main__":
-    GenerateColorImg().generate_img()
+    color_img = GenerateColorImg().generate_img(save_img=False)
+    color_img.show()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from core.generate_color_img import GenerateColorImg
 
 
 def main():
-    color_img = GenerateColorImg().generate_img()
+    color_img = GenerateColorImg().generate_img(add_text=True, save_img=False)
     color_img.show()
 
 
